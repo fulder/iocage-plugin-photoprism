2aaca64be3efaa4d94435da90cf3c805a0f15335
fulder <misad90@gmail.com>
2021-08-21 15:14:12 +0200

    Clone photoprism repo and run make in post_install

--- post_install.sh
+++ post_install.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
 
+echo "Clone photoprism repo"
+git clone https://github.com/huo-ju/photoprism-freebsd-port
+
+echo "Run make install"
+cd photoprism-freebsd-port
+make config
+make && make install
