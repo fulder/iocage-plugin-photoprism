7f6f1250b751851d4048544a9cb4f75af54c5768
fulder <misad90@gmail.com>
2021-08-21 21:36:26 +0200

    Use already present photoprism rc.d file

--- post_install.sh
+++ post_install.sh
@@ -6,14 +6,12 @@ fetch https://github.com/psa/photoprism-freebsd-port/releases/download/2021-05-2
 echo "Install photoprism"
 pkg install -y photoprism-g20210523-FreeBSD-12.2-noAVX.txz
 
-user_name="photoprism"
-
-echo "Creating new user with name: ${user_name}"
-pw useradd -n ${user_name} -c "Photoprism" -s /sbin/nologin -w no
-
 echo "Start photoprism service"
 chmod +x /usr/local/etc/rc.d/photoprism
 sysrc -f /etc/rc.conf photoprism_enable="YES"
+sysrc -f /etc/rc.conf photoprism_assetspath="/var/db/photoprism/assets"
+sysrc -f /etc/rc.conf photoprism_storagepath="/var/db/photoprism/storage"
+sysrc -f /etc/rc.conf photoprism_originalspath="/var/db/photoprism/storage/originals"
 service photoprism start
 
 echo "Post install completed!"
